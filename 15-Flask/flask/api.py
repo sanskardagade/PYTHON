68142ce9096a0ec2e1e28211570bcd104bee691a
sanskardagade <sanskardagade07@gmail.com>
2026-01-01 14:49:56 +0530

    new thing

--- 15-Flask/flask/api.py
+++ 15-Flask/flask/api.py
@@ -5,5 +5,25 @@ from flask import Flask, jsonify,request
 
 app = Flask(__name__)
 
+
+##Initial Data in my to do list 
+items = [
+    {"id": 1, "name": "Item 1", "description": "This is item 1"},
+    {"id": 2, "name": "Item 2", "description": "This is item 2"}
+]
+
+@app.route('/')
+def home
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
