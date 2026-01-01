9ca139cc04c25801a8c6d249e2d9bacd4d6ad1d1
sanskardagade <sanskardagade07@gmail.com>
2026-01-01 09:32:50 +0530

    jinja

---
+++ 15-Flask/flask/api.py
@@ -0,0 +1,9 @@
+### Put and Delete 
+### Working with API's -- JSON 
+
+from flask import Flask, jsonify,request
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- 15-Flask/flask/jinja.py
+++ 15-Flask/flask/jinja.py
@@ -12,7 +12,7 @@
 '''
 
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 
 '''
 It creates an instance of the Flask class,
@@ -35,12 +35,12 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method == 'POST':
-        name=request.form['name']
-        return f'Hello {name}!'
-    return render_template("form.html")
+# @app.route('/submit',methods=['GET','POST'])
+# def submit():
+#     if request.method == 'POST':
+#         name=request.form['name']
+#         return f'Hello {name}!'
+#     return render_template("form.html")
 
 # ## Variable run 
 # @app.route('/success/<score>')
@@ -76,6 +76,36 @@ def successres(score):
     exp={'score':score,'res':res}
 
     return render_template('result1.html',results = exp)
+
+
+## if condition
+@app.route('/successif/<int:score>')
+def successif(score):
+    return render_template('result.html',result=score)
+
+    
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html',result=score)
+
+
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score=total_score))
+
 
 
 ##This is entry point of any py file
