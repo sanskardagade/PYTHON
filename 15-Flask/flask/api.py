7f5f57935fa5905cdb8017cc2e371438a4a9ef0b
sanskardagade <sanskardagade07@gmail.com>
2026-01-02 09:50:35 +0530

    flask done

--- 15-Flask/flask/api.py
+++ 15-Flask/flask/api.py
@@ -13,9 +13,55 @@ items = [
 ]
 
 @app.route('/')
-def home
+def home():
+    return "Welcome to the sample of to do list app"
+
+##Get: Retrive all the items 
+@app.route('/items',methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+##Get: Retrive a sepecific item by id 
+@app.route('/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = next((item for item in items if item["id"]==item_id),None)
+    if item is None:
+        return jsonify({"error":"Item not found"})
+    return jsonify(item)
 
 
+##POST: Create a new task
+
+@app.route('/items',methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error":"Item not found"})
+    new_item = {
+        "id":items[-1]["id"] + 1 if items else 1,
+        "name": request.json['name'],
+        "description":request.json["description"]
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+## Put : Update an existing item
+@app.route('/items/<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item ['id'] == item_id),None)
+    if item is None:
+        return jsonify({"error":"Item not found"})
+
+    item['name'] = request.json.get('name',item['name'])
+    item['description'] = request.json.get('description',item['description'])
+    return jsonify(item)
+
+##DELETE : Delete an item
+
+@app.route('/items/<int:item_id>',methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if item["id"] != item_id] 
+    return jsonify({"result":"Item deleted"})   
 
 
 
